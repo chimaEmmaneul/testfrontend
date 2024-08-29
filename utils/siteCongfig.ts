7e9e7927943746780d6daf8cd68fa0071d0768a8
Emmycruz12 <emmycruz695@gmail.com>
2024-08-29 19:43:22 +0100

    change metadata image

--- utils/siteCongfig.ts
+++ utils/siteCongfig.ts
@@ -2,9 +2,9 @@ export const siteConfig = {
   name: "Online Docs ",
   title: "Its the best on the Market ",
   url: "https://ui.shadcn.com",
-  ogImage: "https://ui.shadcn.com/og.jpg",
-  description:
-    "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+  ogImage:
+    "https://ik.imagekit.io/0xy9wqmrh/Screenshot%202024-08-29%20193846.png?updatedAt=1724956809801",
+  description: "The best shop to get what ever you need",
   links: {
     twitter: "https://twitter.com/shadcn",
     github: "https://github.com/shadcn-ui/ui",
