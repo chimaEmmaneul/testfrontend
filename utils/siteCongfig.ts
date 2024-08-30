fc49749475ca75aea7d8387a34b5672ba5522c6f
Emmycruz12 <emmycruz695@gmail.com>
2024-08-30 09:21:32 +0100

    update siteconfig url

--- utils/siteCongfig.ts
+++ utils/siteCongfig.ts
@@ -6,8 +6,8 @@ export const siteConfig = {
     "https://ik.imagekit.io/0xy9wqmrh/Screenshot%202024-08-29%20193846.png?updatedAt=1724956809801",
   description: "The best shop to get what ever you need",
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn-ui/ui",
+    twitter: "https://twitter.com/frontendlisting.netlify.app",
+    github: "https://github.com/frontendlisting.netlify.app",
   },
 };
 
