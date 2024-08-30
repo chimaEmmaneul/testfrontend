4248e20de9027b2177fccecbd10959c11f8bb348
Emmycruz12 <emmycruz695@gmail.com>
2024-08-30 09:19:16 +0100

    fix siteconfig

--- utils/siteCongfig.ts
+++ utils/siteCongfig.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Online Docs ",
   title: "Its the best on the Market ",
-  url: "https://ui.shadcn.com",
+  url: "https://frontendlisting.netlify.app",
   ogImage:
     "https://ik.imagekit.io/0xy9wqmrh/Screenshot%202024-08-29%20193846.png?updatedAt=1724956809801",
   description: "The best shop to get what ever you need",
